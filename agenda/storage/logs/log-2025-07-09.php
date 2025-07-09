bfe13e3420513cfb62fb9624e60c3b3e7c6741be
subindohostinger <jladislauu@gmail.com>
2025-07-09 20:47:35 +0000

    Novo início do repositório

---
+++ agenda/storage/logs/log-2025-07-09.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-07-09 20:14:42 --> Severity: error --> Exception: Access denied for user 'u292296724_agendateste'@'localhost' (using password: YES) /home/u292296724/domains/lightcyan-starling-554443.hostingersite.com/public_html/agenda/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2025-07-09 20:15:58 --> Severity: error --> Exception: Access denied for user 'u292296724_agendateste'@'localhost' (using password: YES) /home/u292296724/domains/lightcyan-starling-554443.hostingersite.com/public_html/agenda/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2025-07-09 20:16:34 --> 404 Page Not Found: Config/index
